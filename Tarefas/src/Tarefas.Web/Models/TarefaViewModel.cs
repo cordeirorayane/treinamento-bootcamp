0b88f3953e520ab80caf376ca70a28236fd3e60e
Rayane <rayaneee.rj@gmail.com>
2023-01-24 16:33:15 -0300

    Updates

--- Tarefas/src/Tarefas.Web/Models/TarefaViewModel.cs
+++ Tarefas/src/Tarefas.Web/Models/TarefaViewModel.cs
@@ -1,6 +1,11 @@
+using System.ComponentModel;
+
 namespace Tarefas.Web.Models;
 
 public class TarefaViewModel {
+    [DisplayName("Título")]
     public string Titulo {get;set;}
-    public string Descricao {get;set;}
+
+    [DisplayName("Descrição")]
+    public string Descricao {get;set;} 
 }
