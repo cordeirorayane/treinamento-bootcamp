fcccf51dae117001da8d5a92f2f5ea24e8bce7c2
Rayane <rayaneee.rj@gmail.com>
2023-01-24 16:51:45 -0300

    Updates with bootstrap

--- Tarefas/src/Tarefas.Web/Models/TarefaViewModel.cs
+++ Tarefas/src/Tarefas.Web/Models/TarefaViewModel.cs
@@ -3,9 +3,11 @@ using System.ComponentModel;
 namespace Tarefas.Web.Models;
 
 public class TarefaViewModel {
+
     [DisplayName("Título")]
     public string Titulo {get;set;}
 
     [DisplayName("Descrição")]
     public string Descricao {get;set;} 
+    
 }
