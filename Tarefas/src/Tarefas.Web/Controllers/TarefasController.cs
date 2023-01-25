be4bb8114733f70f1bb1224ab385065751484679
talesacademico <talesfelixacademico@gmail.com>
2023-01-25 13:56:34 -0300

    Updates new

--- Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -1,9 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
+using Tarefas.Web.Models;
 
 namespace Tarefas.Web.Controllers;
 
 public class TarefasController : Controller {
     public IActionResult Create(){
         return View();
+    }
+
+    public IActionResult Index(){
+        var listaDeTarefas = new List<TarefaViewModel>(){
+            new TarefaViewModel() {Titulo = "Escovar os dentes", Descricao = "Usar sensodine"},
+            new TarefaViewModel() {Titulo = "Arrumar a cama"},
+            new TarefaViewModel() {Titulo = "Por o lixo pra fora", Descricao = "Somente às terças"}
+        
+        };
+        return View(listaDeTarefas);
     }
 }
